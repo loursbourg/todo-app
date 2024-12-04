029110a411d329e316d1d47c8c9e7c3b4545d303
Kader DAMENE <damene.abdelkader@gmail.com>
2024-12-05 00:05:51 +0100

    handle fetch base errors

--- front-end/src/components/CreateTodoForm.vue
+++ front-end/src/components/CreateTodoForm.vue
@@ -1,24 +1,39 @@
 <script setup lang="ts">
 import { useTodoStore } from '@/stores/todo';
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 
 const todoStore = useTodoStore();
 
 const title = ref('');
+const error = ref<string | null>(null);
+const isLoading = ref(false);
 
-const addTodo = () => {
-    todoStore.addTodo(title.value);
+const addTodo = async () => {
+    try {
+        error.value = null;
+        isLoading.value = true;
+        await todoStore.addTodo(title.value);
+        title.value = '';
+    } catch (e: unknown) {
+        error.value = e instanceof Error ? e.message : 'An unknown error occurred';
+    }
+    isLoading.value = false;
 };
 
-onMounted(() => {
-    todoStore.fetchTodos();
-});
+
 </script>
 
 <template>
+    <div class="result-wrapper">
+        <div class="status-container" v-if="error">
+            <div class="error-alert" role="alert">
+                <div class="error-message">Error: {{ (error) }}</div>
+            </div>
+        </div>
+    </div>
     <div class="todo-input-container">
         <input type="text" v-model="title" class="todo-input" placeholder="Enter your task" aria-label="New Todo" />
-        <button :onclick="addTodo" :disabled="title.length === 0" class="add-todo-btn">
+        <button :onclick="addTodo" :disabled="isLoading || title.length === 0" class="add-todo-btn">
             Add Todo
         </button>
     </div>
--- front-end/src/components/TodoList.vue
+++ front-end/src/components/TodoList.vue
@@ -1,15 +1,30 @@
 <script setup lang="ts">
-import { computed, onMounted, } from 'vue';
+import { computed, onMounted, ref, } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useTodoStore } from '@/stores/todo';
 import TodoItem from '@/components/TodoItem.vue';
 
 const todoStore = useTodoStore()
 
+const error = ref<string | null>(null);
+
+const isLoading = ref(false);
+
 const { filters } = storeToRefs(todoStore)
 
+const fetchTodos = async () => {
+    try {
+        error.value = null;
+        isLoading.value = true;
+        await todoStore.fetchTodos()
+    } catch (e: unknown) {
+        error.value = e instanceof Error ? e.message : 'An unknown error occurred';
+    }
+    isLoading.value = false;
+}
+
 onMounted(() => {
-    todoStore.fetchTodos()
+    fetchTodos()
 })
 
 const filteredTodos = computed(() => {
@@ -25,6 +40,13 @@ const filteredTodos = computed(() => {
 </script>
 
 <template>
+    <div class="result-wrapper">
+        <div class="status-container" v-if="error">
+            <div class="error-alert" role="alert">
+                <div class="error-message">Error: {{ (error) }}</div>
+            </div>
+        </div>
+    </div>
     <div>
         <TodoItem v-for="todo in filteredTodos" :key="todo.id" :todo="todo" :toggle="todoStore.toggleTodo" />
     </div>

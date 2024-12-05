fa2d1198c34a93b30b35033ff77bf30175fc5918
Kader DAMENE <damene.abdelkader@gmail.com>
2024-12-05 02:19:03 +0100

    fetch todos only once

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,17 +1,8 @@
 <script setup lang="ts">
-import { onMounted, } from 'vue';
-import { useTodoStore } from '@/stores/todo';
 import AppHeader from '@/components/AppHeader.vue';
 import CreateTodoForm from '@/components/CreateTodoForm.vue';
 import FiltersSection from '@/components/FiltersSection.vue';
 import TodoList from '@/components/TodoList.vue';
-
-const todoStore = useTodoStore()
-
-onMounted(() => {
-  todoStore.fetchTodos()
-})
-
 </script>
 
 <template>
